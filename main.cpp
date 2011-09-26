21fb6d5cb41becc9045d246df88f6c4005603c58
Evgeny Sinelnikov <sin@altlinux.ru>
2011-09-26 06:19:50 +0400

    Add two QPushButtons to empty window

--- main.cpp
+++ main.cpp
@@ -7,7 +7,19 @@ int main(int argc, char *argv[])
     QApplication app(argc,argv);
 
     QWidget *widget = new QWidget;
+    QPushButton *quitButton = new QPushButton("Quit");
+    QPushButton *aboutButton = new QPushButton("About QT");
 
+    QVBoxLayout *buttonLayout = new QVBoxLayout;
+    buttonLayout->addWidget(quitButton);
+    buttonLayout->addWidget(aboutButton);
+
+    QObject::connect(quitButton,SIGNAL(clicked()),qApp,SLOT(quit()));
+    QObject::connect(aboutButton,SIGNAL(clicked()),qApp,SLOT(aboutQt()));
+
+    widget->setLayout(buttonLayout);
+    widget->setWindowFlags(Qt::Window);
+    widget->show();
     widget->show();
 
     return app.exec();
