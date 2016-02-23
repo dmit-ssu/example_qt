12359949c9b3c2c062292ae8c31a0982a51259c6
Evgeny Sinelnikov <sin@altlinux.ru>
2016-02-23 03:11:34 +0300

    Add class Task as QObject subclass

--- main.cpp
+++ main.cpp
@@ -1,13 +1,38 @@
 #include <iostream>
 #include <QCoreApplication>
+#include <QTimer>
 
 using namespace std;
+
+class Task: public QObject
+{
+    Q_OBJECT
+public:
+    Task(QObject *parent = 0) : QObject(parent) {}
+
+public slots:
+    void run()
+    {
+        cout << "Hello, World!\n";
+
+        emit finished();
+    }
+
+signals:
+    void finished();
+};
+
+#include "main.moc"
 
 int main(int argc, char *argv[])
 {
     QCoreApplication app(argc, argv);
 
-    cout << "Hello, World!\n";
+    Task *task = new Task(&app);
+
+    QObject::connect(task, SIGNAL(finished()), &app, SLOT(quit()));
+
+    QTimer::singleShot(500, task, SLOT(run()));
 
     return app.exec();
 }
